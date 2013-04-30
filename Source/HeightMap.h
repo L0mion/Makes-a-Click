82f4196aedff3b09e4f7061363786204e509a0f3
Mattias Liljeson <mattiasliljeson+github@gmail.com>
2013-04-30 14:08:42 +0200

    Further refactoring of renderer and heightmap.

--- Source/HeightMap.h
+++ Source/HeightMap.h
@@ -1,41 +1,40 @@
-#ifndef HEIGHTMAP_H
-#define HEIGHTMAP_H
+#pragma once
 
 #include <vector>
 #include <fstream>
 using namespace std;
 
-//#include "D3DObject.h"
-//#include "Mesh.h"
-//#include "Surface.h"
 #include "Vertex.h"
-//#include "GameObject.h"
+
+struct EntityBufferInfo;
 
 class HeightMap
 {
+public:
+	HeightMap();
+	~HeightMap();
+	//vector<Model> createModels(/*int pTechNr = 0, int pPassNr = 0, */int maxSize = 256);
+	float getHeight(float p_x, float p_z);
+	EntityBufferInfo* getEntityBufferInfo();
+
 private:
-	//System* system;
-
-	float cellSize;
-	int nrOfCols;
-	int nrOfRows;
-	int nrOfFaces;
-	vector<float> heightMap;
-
-	//Heightmap specific
-	void loadHeightMap(int numVertices);
+	// Heightmap specific
+	void loadHeightMap(int p_vertexCnt);
 	void smoothHeightMap();
-	float average(int posRow, int posCol);
+	float average(int p_posRow, int p_posCol);
 	bool inBounds(int i, int j);
 
-	// Generate Model
-	vector<HeightMapVertex> defineVertexBuffer(int numVertices);
-	vector<int> defineIndexBuffer(int numIndices);
+	// Generate EntityBufferInfo
+	void createEntityBufferInfo();
+	vector<HeightMapVertex> defineVertexBuffer( int p_vertexCnt );
+	vector<int> defineIndexBuffer( int p_indexCnt );
 
-public:
-	HeightMap(/*System* pSystem*/);
-	//vector<Model> createModels(/*int pTechNr = 0, int pPassNr = 0, */int maxSize = 256);
-	float getHeight(float x, float z);
+private: // Variables
+	float m_cellSize;
+	int m_colCnt;
+	int m_rowCnt;
+	int m_faceCnt;
+	vector<float> m_heightMap;
+
+	EntityBufferInfo* m_bufferInfo;
 };
-
-#endif //HEIGHTMAP_H
